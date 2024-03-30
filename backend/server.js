ccbcc5ed88ed98ff535bbb6e3fc3e426dff3ef05
shahid <shahidazhari313@gmail.com>
2024-03-30 15:42:18 +0530

    mongodb connected

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
-import express from "express"
-import dotenv from "dotenv"
-dotenv.config()
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/connectDb.js";
 
-const PORT = process.env.PORT || 5000
-const app = express() 
+dotenv.config();
+connectDB();
 
-app.listen(PORT, ()=> console.log(`Server started @ http://localhost:${PORT} `))
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.listen(PORT, () =>
+  console.log(`Server started @ http://localhost:${PORT} `)
+);
