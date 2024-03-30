c8e9678f2bc8ae4cc6b3c394612e0865afe36834
shahid <shahidazhari313@gmail.com>
2024-03-30 14:55:34 +0530

    server started

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+const app = express() 
+
+app.listen(PORT, ()=> console.log(`Server started @ http://localhost:${PORT} `))
