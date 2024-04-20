6573997fbb268fb0cd44fa8ba13f04aeb00f1203
shahid <shahidazhari313@gmail.com>
2024-04-20 08:51:45 +0530

    socket server ok

--- backend/server.js
+++ backend/server.js
@@ -6,19 +6,19 @@ import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 import messageRoute from "./routes/messageRoute.js"
 import { v2 as cloudinary } from "cloudinary";
+import { server, app } from "./socket/socket.js";
 
 dotenv.config();
 connectDB();
 
 const PORT = process.env.PORT || 5000;
-const app = express();
-
+ 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
+ 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -27,6 +27,6 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/messages", messageRoute);
 
-app.listen(PORT, () =>
+server.listen(PORT, () =>
   console.log(`Server started @ http://localhost:${PORT} `)
 );
